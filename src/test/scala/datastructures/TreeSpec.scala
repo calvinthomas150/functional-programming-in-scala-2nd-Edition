36c81a3e2ae418bdc8a31941c6d28115be15f8e3
Calvin Thomas <calvinthomas@protonmail.com>
2023-06-11 12:55:31 +0100

    Chapter 3 minor fixes

--- src/test/scala/datastructures/TreeSpec.scala
+++ src/test/scala/datastructures/TreeSpec.scala
@@ -12,19 +12,19 @@ class TreeSpec extends AnyFlatSpec with should.Matchers {
     tree.maximum should be (5)
   }
 
-  "it" should "have size equal to the number of branches and leaves in the tree" in {
+  it should "have size equal to the number of branches and leaves in the tree" in {
     tree.size should be (5)
   }
 
-  "it" should "give the maximum depth of the tree from the root node via the depth function" in {
+  it should "give the maximum depth of the tree from the root node via the depth function" in {
     tree.depth should be (2)
   }
 
-  "it" should "allow the map function to apply a function to all the values in every Leaf" in {
+  it should "allow the map function to apply a function to all the values in every Leaf" in {
     tree.map(_ + 1) should be (Branch(Branch(Leaf(2), Leaf(6)), Leaf(4)))
   }
 
-  "it" should "give the same results for v2 of size, depth, maximum and map functions as the original versions" in {
+  it should "give the same results for v2 of size, depth, maximum and map functions as the original versions" in {
     tree.size should be (tree.size_v2)
     tree.depth should be (tree.depth_v2)
     tree.maximum should be (tree.maximum_v2)
